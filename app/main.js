1b2d9ee2966527e6be7c8e016f7098f34a5d610d
Maria Fomashina <m.fomashina@corp.mail.ru>
2020-06-09 12:45:09 +0300

    screen capture example

--- app/main.js
+++ app/main.js
@@ -1,17 +1,47 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, session, ipcMain } = require('electron');
+const path = require('path');
 
 let mainWindow = null;
+let offscreenWindow = null;
 
 app.on('ready', () => {
-  mainWindow = new BrowserWindow({ show: false });
+  mainWindow = new BrowserWindow({ show: false, webPreferences: { nodeIntegration: true, contextIsolation: false } });
+  offscreenWindow = new BrowserWindow({ show: false, webPreferences: { nodeIntegration: true, contextIsolation: false } });
 
   mainWindow.loadURL(`file://${__dirname}/index.html`);
+  // offscreenWindow.loadURL(`https://animejs.com/`);
+  // offscreenWindow.loadURL('https://www.youtube.com/watch?v=IUCJzJMhHCg');
+  offscreenWindow.show();
+  // offscreenWindow.loadURL('https://blankslate.io/');
+  offscreenWindow.loadURL('https://simpl.info/videoalpha/video/soccer1.webm');
 
   mainWindow.once('ready-to-show', () => {
     mainWindow.show();
+    mainWindow.webContents.toggleDevTools();
+  });
+
+  ipcMain.on('capture', () => {
+    if (offscreenWindow) {
+      const focused = BrowserWindow.getFocusedWindow();
+      console.log(focused);
+
+      if (focused) {
+        const offscreen = BrowserWindow.getAllWindows().find((window) => window.id !== focused.id);
+
+        if (offscreen) {
+          focused.webContents.send('window-id', { id: offscreen.id });
+        }
+      }
+    }
   });
 
   mainWindow.on('closed', () => {
     mainWindow = null;
+    offscreenWindow = null;
   });
+
+  // const extPath = path.join(__dirname, 'lib', 'extension');
+  // console.log(extPath);
+
+  // await session.defaultSession.loadExtension(extPath);
 });
--- app/renderer.js
+++ app/renderer.js
@@ -1,9 +1,85 @@
-const marked = require('marked');
+const { ipcRenderer, desktopCapturer } = require('electron');
 
-const markdownView = document.querySelector('#markdown');
-const htmlView = document.querySelector('#html');
-const newFileButton = document.querySelector('#new-file');
-const openFileButton = document.querySelector('#open-file');
-const saveMarkdownButton = document.querySelector('#save-markdown');
-const revertButton = document.querySelector('#revert');
-const saveHtmlButton = document.querySelector('#save-html');
+const handleStream = (stream) => {
+    const video = document.querySelector('#video');
+    video.srcObject = stream;
+
+    const videoTrack = video.srcObject.getVideoTracks()[0];
+    console.info("Track settings:");
+    console.info(JSON.stringify(videoTrack.getSettings(), null, 2));
+    console.info("Track constraints:");
+    console.info(JSON.stringify(videoTrack.getConstraints(), null, 2));
+};
+
+const handleError = (error) => {
+  debugger
+};
+
+window.onload = () => {
+    const button = document.querySelector('#button');
+    button.addEventListener('click', () => {
+        ipcRenderer.send('capture');
+    });
+
+    const canvas = document.querySelector('#img') ;
+
+    ipcRenderer.on('window-id', (event, data) => {
+        const id = data.id;
+
+        desktopCapturer.getMediaSourceIdForWebContents(id).then(async (mediaSourceId) => {
+            console.log('mediaSourceId', mediaSourceId);
+
+            try {
+                const stream = await navigator.mediaDevices.getUserMedia({
+                  audio: {
+                    mandatory: {
+                      chromeMediaSource: 'tab',
+                      chromeMediaSourceId: mediaSourceId
+                    }
+                  },
+                  video: {
+                    mandatory: {
+                      chromeMediaSource: 'tab',
+                      chromeMediaSourceId: mediaSourceId,
+                      minWidth: 1280,
+                      maxWidth: 1280,
+                      minHeight: 720,
+                      maxHeight: 720,
+                    }
+                  }
+                })
+                handleStream(stream)
+
+                const track = stream.getVideoTracks()[0];
+                const imageCapture = new ImageCapture(track);
+
+                const img = await imageCapture.grabFrame();
+
+                canvas.width = getComputedStyle(canvas).width.split('px')[0];
+                canvas.height = getComputedStyle(canvas).height.split('px')[0];
+
+                let ratio  = Math.min(canvas.width / img.width, canvas.height / img.height);
+                let x = (canvas.width - img.width * ratio) / 2;
+                let y = (canvas.height - img.height * ratio) / 2;
+
+                const bufferCanvas = new OffscreenCanvas(canvas.width, canvas.height);
+
+                bufferCanvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+                bufferCanvas.getContext('2d').fillStyle = 'red';
+                bufferCanvas.getContext('2d').fillRect(0, 0, canvas.width, canvas.height);
+                bufferCanvas.getContext('2d').drawImage(img, 0, 0, img.width, img.height,
+                    x, y, img.width * ratio, img.height * ratio);
+
+                const imageData = bufferCanvas.getContext('2d').getImageData(0, 0, canvas.width, canvas.height)
+
+                canvas.getContext('2d').putImageData(imageData, 0, 0, 0, 0, canvas.width, canvas.height);
+
+                for (let i = 0, len = imageData.data.length; i < len; i = i + 4) {
+                  console.log(imageData.data[i], imageData.data[i + 1], imageData.data[i + 2], imageData.data[i + 3])
+                }
+              } catch (e) {
+                handleError(e)
+              }
+        });
+    });
+};
